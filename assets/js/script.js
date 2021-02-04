9b10b37fd28626a850c63ccf507880a0d1cc927c
Aaliyah <aaliyah.symlar@vanderbilt.edu>
2021-02-04 11:32:19 -0600

    added container to hold api data.  added new api request to js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,7 @@
+// variable for fetch call element
+var track = document.querySelector(".container");
+
+
 //modal code
 // adds an active class when the Submit button is clicked
 $(".submitBtn").on("click", function() {
@@ -9,21 +13,27 @@ $(".close, .modal").on("click", function() {
     $(".modal, .modal-content").removeClass("active");
 });
 
-fetch("https://api.deezer.com/artist/486308/top?limit=50", {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-key": "d0f3f1eba7msh430ab68207dc835p1cacebjsnab2cb1117987",
-        "x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com"
-        
-    }
-    
-})
-.then(response => {
-    console.log(response);
-    
-    const li = document.createElement("li");
-})
+const data = null;
 
-.catch(err => {
-	console.error(err);
+const xhr = new XMLHttpRequest();
+xhr.withCredentials = true;
+
+xhr.addEventListener("readystatechange", function () {
+	if (this.readyState === this.DONE) {
+		
+	}
 });
+
+xhr.open("GET", "https://deezerdevs-deezer.p.rapidapi.com/search?q=allen%20stone");
+xhr.setRequestHeader("x-rapidapi-key", "d0f3f1eba7msh430ab68207dc835p1cacebjsnab2cb1117987");
+xhr.setRequestHeader("x-rapidapi-host", "deezerdevs-deezer.p.rapidapi.com");
+
+xhr.send(data);
+
+const div = document.createElement("div");
+
+div.classList.add("link");
+const markup = `<h2 class="track_short"`;
+
+div.innerHTML = markup;
+track.appendChild(div);
