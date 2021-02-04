58afb3a731d4f56e5c400fb5033c28ca8a222c3c
Aaliyah <aaliyah.symlar@vanderbilt.edu>
2021-02-04 10:56:38 -0600

    added new fetch call to js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,3 +9,21 @@ $(".close, .modal").on("click", function() {
     $(".modal, .modal-content").removeClass("active");
 });
 
+fetch("https://api.deezer.com/artist/486308/top?limit=50", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-key": "d0f3f1eba7msh430ab68207dc835p1cacebjsnab2cb1117987",
+        "x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com"
+        
+    }
+    
+})
+.then(response => {
+    console.log(response);
+    
+    const li = document.createElement("li");
+})
+
+.catch(err => {
+	console.error(err);
+});
