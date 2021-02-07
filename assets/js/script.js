b49d15d79dc1953251d27cbc1ea3ddf912f1474b
Dakota <piercedakota26@gmail.com>
2021-02-07 14:41:20 -0600

    updated insta api

--- assets/js/script.js
+++ assets/js/script.js
@@ -56,20 +56,20 @@ fetch("https://instagram40.p.rapidapi.com/account-feed?username=allenstone", {
 })
     .then(response => response.json())
     .then(response => {
-        var instaData = response.data;
-        console.log(instaData);
-
-        for (var i = 0; i < instaData.length; i++) {
-            var instaLink = document.createElement("b")
-            instaLink.textContent = instaData[i].title;
-            instaLink.href = instaData[i].preview;
-            document.getElementById("feed-here")
-            feedContainer.appendChild(instaLink)
-        }
+        console.log(response);
+        var feed = response.data;
+        for (var i = 0; i < feed.length; i++) {
+            var link = document.createElement("a");
+            link.textContent = feed[i].title;
+            link.href = feed[i].preview;
+            document.getElementById("feed-here");
+            feedContainer.appendChild(link);
+        } 
     })
+
     .catch(err => {
         console.error(err);
-    });
+});
 
 
 
